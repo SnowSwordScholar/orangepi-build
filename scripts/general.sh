8e2280df6efef4c855661cbafbdf10d28925943e
SnowSwordScholar <ink-star@outlook.com>
2025-08-30 22:09:59 +0800

    可以正常使用本身的 Armbian 进行编译

--- scripts/general.sh
+++ scripts/general.sh
@@ -1460,13 +1460,9 @@ prepare_host()
 	#
 	# NO_HOST_RELEASE_CHECK overrides the check for a supported host system
 	# Disable host OS check at your own risk. Any issues reported with unsupported releases will be closed without discussion
-	if [[ -z $HOSTRELEASE || "focal jammy noble" != *"$HOSTRELEASE"* ]]; then
-		if [[ $NO_HOST_RELEASE_CHECK == yes ]]; then
-			display_alert "You are running on an unsupported system" "${HOSTRELEASE:-(unknown)}" "wrn"
-			display_alert "Do not report any errors, warnings or other issues encountered beyond this point" "" "wrn"
-		else
-			exit_with_error "It seems you ignore documentation and run an unsupported build system: ${HOSTRELEASE:-(unknown)}"
-		fi
+	# System version check has been removed to support more distributions
+	if [[ -z $HOSTRELEASE ]]; then
+		display_alert "Running on system" "${HOSTRELEASE:-(unknown)}" "info"
 	fi
 
 	if grep -qE "(Microsoft|WSL)" /proc/version; then
